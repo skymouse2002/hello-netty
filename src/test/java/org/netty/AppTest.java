2fa1505668359ef0d3418ba6c0657ed87a9c4ec8
kevin <hao_3602@163.com>
2021-02-03 12:13:52 +0800

    Initial commit

---
+++ src/test/java/org/netty/AppTest.java
@@ -0,0 +1,20 @@
+package org.netty;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
